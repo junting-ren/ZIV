f1d6e7a8c24815db8d11dd44d95cf4e7aaf34446
Junting <59756098+junting-ren@users.noreply.github.com>
2022-12-30 17:06:56 -0800

    Updated the parameter names to match with the paper
    Updated the parameter names to match with the paper

--- functions/full_slab_spike_model_constructor.py
+++ functions/full_slab_spike_model_constructor.py
@@ -18,7 +18,8 @@ def cust_act(x):
 class linear_slab_spike(nn.Module):
     def __init__(self, p, n_total, init_pi_local = 0.45, init_pi_global = 0.5, init_beta_var = 1, init_noise_var = 1,
                 gumbel_softmax_temp = 0.5, gumbel_softmax_hard = False, a1= 1.1,a2=3.1, init_a3= 1.1, init_a4 = 5.1,
-                q1 = 1.1, q2 = 1.1, init_q3 = 1.1, init_q4 = 1.1, n_E = 50, prior_sparsity = False, device = 'cpu'):
+                b1 = 1.1, b2 = 1.1, init_b3 = 1.1, init_b4 = 1.1, n_E = 50, prior_sparsity = True,
+                 prior_sparsity_beta = False, device = 'cpu'):
         '''Initialize fast variational inference Bayesian slab and spike linear model
         
         Parameters:
@@ -34,12 +35,13 @@ class linear_slab_spike(nn.Module):
         a2: prior beta distribution parameters for global sparsity
         init_a3: posterior beta distribution parameter
         init_a4:posterior beta distribution parameter
-        q1: prior inverse gamma distribution parameters for noise varaiance
-        q2: prior inverse gamma distribution parameters for noise varaiance
-        init_q3: posterior inverse gamma distribution parameters for noise varaiance
-        init_q4: posterior inverse gamma distribution parameters for noise varaiance
+        b1: prior inverse gamma distribution parameters for noise varaiance
+        b2: prior inverse gamma distribution parameters for noise varaiance
+        init_b3: posterior inverse gamma distribution parameters for noise varaiance
+        init_b4: posterior inverse gamma distribution parameters for noise varaiance
         n_E: number of samples for the empirical expectation of the data likelihood
         prior_sparsity: If True, the global sparsity will have a prior or else it will be estimated through Maximum likelihood.
+        prior_sparsity_beta: If True, the global sparsity will have a beta prior or else it will be Uniform.
         
         '''
         super(linear_slab_spike, self).__init__()
@@ -50,20 +52,21 @@ class linear_slab_spike(nn.Module):
         prior_uni = np.sqrt(6/p) # initial values for coefficient mean 
         self.a1 = torch.tensor((a1,)).to(device)# prior parameters for global pi
         self.a2 = torch.tensor((a2,)).to(device)# prior parameters for global pi
-        self.q1 = torch.tensor((q1,)).to(device)#Priors parameters for noise variance
-        self.q2 = torch.tensor((q2,)).to(device)#Priors parameters for noise variance
+        self.b1 = torch.tensor((b1,)).to(device)#Priors parameters for noise variance
+        self.b2 = torch.tensor((b2,)).to(device)#Priors parameters for noise variance
         self.tau = gumbel_softmax_temp# gumbel hyper parameters        
         self.hard = gumbel_softmax_hard# gumbel hyper parameters
         self.n_E = n_E #number of samples for empirical integration
         self.prior_sparsity = prior_sparsity # If True, the global sparsity will have a prior or else it will be estimated through Maximum likelihood.
+        self.prior_sparsity_beta = prior_sparsity_beta
         # Variational approximation posterior distribution parameters
         self.beta_mu = nn.Parameter(torch.FloatTensor(size = (p,)).uniform_(-prior_uni,prior_uni)) # beta mean:Xavier Uniform Distribution initilization
         self.beta_log_var = nn.Parameter(torch.log(torch.rand((p,)))) # beta log variance: uniform 0,1 initilizaiton
         self.logit_pi_local = nn.Parameter(torch.logit(torch.FloatTensor(size = (p,)).uniform_(init_pi_local-0.05,init_pi_local+0.05))) # sparsity for each feature, uniform initilized
         self.log_a3 = nn.Parameter(torch.log(torch.tensor((init_a3,)))) # prior for global pi beta distribution
         self.log_a4 =  nn.Parameter(torch.log(torch.tensor((init_a4,))))# prior for global pi beta distribution
-        self.log_q3 = nn.Parameter(torch.log(torch.tensor((init_q3,))))# prior for noise variance inverse gamma distribution
-        self.log_q4 = nn.Parameter(torch.log(torch.tensor((init_q4,))))# prior for noise variance inverse gamma distribution
+        self.log_b3 = nn.Parameter(torch.log(torch.tensor((init_b3,))))# prior for noise variance inverse gamma distribution
+        self.log_b4 = nn.Parameter(torch.log(torch.tensor((init_b4,))))# prior for noise variance inverse gamma distribution
         # MLE parameters
         self.bias = nn.Parameter(torch.tensor((1.,)))# Bias
         self.logit_pi_global = nn.Parameter(torch.logit(torch.tensor(init_pi_global))) # global pi on logit scale 
@@ -76,9 +79,9 @@ class linear_slab_spike(nn.Module):
         return torch.exp(self.log_var_noise),torch.exp(self.beta_log_var), cust_act(self.logit_pi_local), \
                 torch.exp(self.beta_log_var_prior), \
                 torch.exp(self.log_a3), torch.exp(self.log_a4),\
-                torch.exp(self.log_q3),torch.exp(self.log_q4), cust_act(self.logit_pi_global)
+                torch.exp(self.log_b3),torch.exp(self.log_b4), cust_act(self.logit_pi_global)
     
-    def log_data_lh(self, beta, delta, X, y, q3, q4):
+    def log_data_lh(self, beta, delta, X, y, b3, b4):
         '''Calculate the expected data likelihood over the approximation posterior
         
         Parameters:
@@ -87,8 +90,8 @@ class linear_slab_spike(nn.Module):
         delta: sampled sparsity, n_E by p matrix
         X: feature matrix, n by p
         y: outcome vector, n by 1
-        q3: noise variance inverse gamma for approximation posterior
-        q4: noise variance inverse gamma for approximation posterior
+        b3: noise variance inverse gamma for approximation posterior
+        b4: noise variance inverse gamma for approximation posterior
         
         Return:
         ----------------------
@@ -98,9 +101,9 @@ class linear_slab_spike(nn.Module):
         #import pdb;pdb.set_trace()
         est_mean = (beta*delta) @ X.t()+self.bias
         #import pdb;pdb.set_trace()
-        return torch.mean(-n*0.5*(torch.log(q4)-torch.digamma(q3))-1/(2)*q3/q4*torch.sum(torch.square(y-est_mean),dim = 1))
+        return torch.mean(-n*0.5*(torch.log(b4)-torch.digamma(b3))-1/(2)*b3/b4*torch.sum(torch.square(y-est_mean),dim = 1))
     
-    def log_prior_expect_lh(self, pi_local, beta_var, beta_var_prior, a3, a4, q3, q4, pi_global):
+    def log_prior_expect_lh(self, pi_local, beta_var, beta_var_prior, a3, a4, b3, b4, pi_global):
         '''Calculate the expected data likelihood over the approximation posterior
         
         Parameters:
@@ -110,8 +113,8 @@ class linear_slab_spike(nn.Module):
         beta_var_prior: fixed prior variance for coefficient
         a3: global sparsity beta distribution parameter for approximation posterior
         a4: global sparsity beta distribution parameter for approximation posterior
-        q3: noise variance inverse gamma for approximation posterior
-        q4: noise variance inverse gamma for approximation posterior
+        b3: noise variance inverse gamma for approximation posterior
+        b4: noise variance inverse gamma for approximation posterior
         pi_global: global sparsity if prior_sparsity=False, or else it is not used
         
         Return:
@@ -119,8 +122,8 @@ class linear_slab_spike(nn.Module):
         Expected prior likelihood over the approximation posterior
         '''
         # expected log likelihood of noise variance
-        lh_noise_var = self.q1*torch.log(self.q2)-torch.lgamma(self.q1)\
-        -(self.q1+1)*(torch.log(q4)-torch.digamma(q3)) - self.q2*q3/q4
+        lh_noise_var = self.b1*torch.log(self.b2)-torch.lgamma(self.b1)\
+        -(self.b1+1)*(torch.log(b4)-torch.digamma(b3)) - self.b2*b3/b4
         if self.prior_sparsity:
             # expectation of log pi
             expect_lpi = torch.digamma(a3)-torch.digamma(a3 + a4)
@@ -132,7 +135,10 @@ class linear_slab_spike(nn.Module):
             - self.p*0.5*self.beta_log_var_prior \
             - 0.5*(torch.sum(beta_var)+torch.sum(torch.square(self.beta_mu)))/beta_var_prior
             # expected log likelihood of global pi part
-            lh_global_pi = (self.a1-1)*expect_lpi+(self.a2 - 1)* expect_l1_pi - torch.lgamma(self.a1)-torch.lgamma(self.a2)+torch.lgamma(self.a1+self.a2)
+            if self.prior_sparsity_beta:
+                lh_global_pi = (self.a1-1)*expect_lpi+(self.a2 - 1)* expect_l1_pi - torch.lgamma(self.a1)-torch.lgamma(self.a2)+torch.lgamma(self.a1+self.a2)
+            else:
+                lh_global_pi = 0 # since log(1)=0 for uniform
             return lh_1+lh_global_pi+lh_noise_var
         else:
             lh1 = torch.sum(torch.log(pi_global)*pi_local\
@@ -141,7 +147,7 @@ class linear_slab_spike(nn.Module):
             - 0.5*(torch.sum(beta_var)+torch.sum(torch.square(self.beta_mu)))/beta_var_prior
             return lh1+lh_noise_var
     
-    def log_entropy(self, pi_local, a3, a4, q3, q4, pi_global):
+    def log_entropy(self, pi_local, a3, a4, b3, b4, pi_global):
         '''Calculate the entropy for the approximation posterior
         
         Parameters:
@@ -149,8 +155,8 @@ class linear_slab_spike(nn.Module):
         pi_local: sparsity parameter for approximation posterior: p by 1
         a3: global sparsity beta distribution parameter for approximation posterior
         a4: global sparsity beta distribution parameter for approximation posterior
-        q3: noise variance inverse gamma for approximation posterior
-        q4: noise variance inverse gamma for approximation posterior
+        b3: noise variance inverse gamma for approximation posterior
+        b4: noise variance inverse gamma for approximation posterior
         pi_global: global sparsity if prior_sparsity=False, or else it is not used
         
         Return:
@@ -165,7 +171,7 @@ class linear_slab_spike(nn.Module):
                                 - (a4 - 1)*torch.digamma(a4)+(a3+a4-2)*torch.digamma(a3+a4)
         else:
             entropy_global_pi = 0
-        entropy_noise_var = q3+torch.log(q4)+torch.lgamma(q3)-(q3+1)*torch.digamma(q3)
+        entropy_noise_var = b3+torch.log(b4)+torch.lgamma(b3)-(b3+1)*torch.digamma(b3)
         return entropy1+entropy_global_pi+entropy_noise_var
     
     def ELBO(self,X, y, B):
@@ -178,16 +184,16 @@ class linear_slab_spike(nn.Module):
         '''
         n_batch = X.shape[0]
         # get the current parameter after transformation
-        noise_var, beta_var, pi_local,beta_var_prior,a3, a4, q3, q4,pi_global = self.get_para_orig_scale()
+        noise_var, beta_var, pi_local,beta_var_prior,a3, a4, b3, b4,pi_global = self.get_para_orig_scale()
         # reparameterization
         #import pdb; pdb.set_trace()
         beta = self.beta_mu + torch.sqrt(beta_var)*torch.randn((self.n_E,self.p), device = self.device)
         # Gumbel-softmax sampling
         delta = (nn.functional.gumbel_softmax(torch.stack( [ self.logit_pi_local.expand(self.n_E ,-1), -self.logit_pi_local.expand(self.n_E,-1) ], dim = 2 ),dim = 2, tau = self.tau, hard = self.hard)[:,:,0])
         # ELBO
-        ELBO = self.log_data_lh(beta, delta, X, y, q3, q4) + \
-            n_batch/self.n_total*self.log_prior_expect_lh(pi_local, beta_var, beta_var_prior, a3, a4, q3, q4,pi_global) + \
-            n_batch/self.n_total*self.log_entropy(pi_local, a3, a4, q3, q4, pi_global)
+        ELBO = self.log_data_lh(beta, delta, X, y, b3, b4) + \
+            n_batch/self.n_total*self.log_prior_expect_lh(pi_local, beta_var, beta_var_prior, a3, a4, b3, b4,pi_global) + \
+            n_batch/self.n_total*self.log_entropy(pi_local, a3, a4, b3, b4, pi_global)
         return ELBO
     
     def inference(self, X, num_samples = 500, plot = False, true_beta = None):
@@ -213,10 +219,10 @@ class linear_slab_spike(nn.Module):
         sample_beta = np.random.normal(loc = beta_mean, scale = beta_std, size = (num_samples, self.p))*delta # num_samples* p
         est_mean = X.numpy() @ np.transpose(sample_beta) + self.bias.cpu().detach().numpy() # a n*num_samples matrix
         # Noise variance poseterior parameters
-        q3 = np.exp(self.log_q3.cpu().detach().numpy())
-        q4 = np.exp(self.log_q4.cpu().detach().numpy())
+        b3 = np.exp(self.log_b3.cpu().detach().numpy())
+        b4 = np.exp(self.log_b4.cpu().detach().numpy())
         # Noise variance posterior
-        noise_var_poster = 1/np.random.gamma(q3,1/q4, size = (num_samples,))
+        noise_var_poster = 1/np.random.gamma(b3,1/b4, size = (num_samples,))
         noise_var_est = np.mean(noise_var_poster)
         # posterior for h
         var_genetic_est = np.mean(est_mean**2, axis = 0) - np.mean(est_mean, axis = 0)**2
