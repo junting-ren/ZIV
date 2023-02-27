e65f6997064d1113cef2fbb7a19977d6528b5ef2
juntingren <59756098+junting-ren@users.noreply.github.com>
2023-02-27 14:39:48 -0800

    Update mcmc_linear.py
    Included the tobit model code for MCMC algorithm

--- functions/mcmc_linear.py
+++ functions/mcmc_linear.py
@@ -4,9 +4,9 @@ from numpyro.infer import MCMC, NUTS,SA,HMCECS, init_to_median, MixedHMC, HMC,Di
 import numpyro
 import jax.numpy as jnp
 import numpy as np
-
+from tobit_dist import tobit_dist
 class experiment(object):
-    def __init__(self, model, z, X,use_gpu = False):
+    def __init__(self, model, z, X,use_gpu = False, tobit = False):
         self.model = model
         self.z = z
         self.X = X
@@ -58,5 +58,40 @@ def linear_mcmc_model(x, z):
     #     numpyro.sample("obs", tobit_dist(latent_mean, jnp.sqrt(var_error)), obs = z)
     with numpyro.plate("data", x.shape[0]):
         numpyro.sample("obs", dist.Normal(latent_mean, jnp.sqrt(var_error)), obs = z)
-    #return zl_probit_mix_trun_norm_dist(p = model_prob, locs = locs, scale = scale_mix_trunc, start =start, end = end).sample()
+    return h
+
+
+def tobit_mcmc_model(x, z):
+    # Global variables.
+    N, d = x.shape
+    #var_beta =  pyro.sample('var_beta', dist.InverseGamma(5, 0.1))
+    var_beta =  numpyro.sample('var_beta', dist.InverseGamma(1, 1))
+    #var_lambda =  numpyro.sample('var_lambda', dist.InverseGamma(1, 1))
+    pi =  numpyro.sample('pi', dist.Beta(0.5, 0.5))
+    var_bias = numpyro.sample('var_beta_bias', dist.InverseGamma(1,1))
+    bias = numpyro.sample('beta_bias', dist.Normal(loc=0, scale=jnp.sqrt(var_bias)))
+    var_error = numpyro.sample('var_error', dist.InverseGamma(0.1,0.1))
+    #c = torch.tensor(1)
+    with numpyro.plate('local_shrinkage', d):
+        #Lambda = numpyro.sample('lambda', dist.HalfCauchy(scale=jnp.sqrt(var_lambda)))
+        #Lambda = pyro.sample('lambda', dist.HalfCauchy(scale=1))
+        # Traditional horseshoe
+        #horseshoe_sigma = jnp.sqrt(var_beta)*Lambda
+        horseshoe_sigma = jnp.sqrt(var_beta)
+        #Regularized horseshoe
+        # scale_beta = torch.sqrt(var_beta)
+        # horseshoe_sigma = torch.sqrt(c**2*Lambda**2/(c**2+scale_beta**2*Lambda**2))*scale_beta
+        delta = numpyro.sample('delta', dist.Bernoulli(probs=pi))
+        beta = numpyro.sample('beta', dist.Normal(loc=0, scale=horseshoe_sigma))
+    #import pdb; pdb.set_trace()
+    latent_mean = jnp.dot(x, beta*delta)+bias
+    #y_star = numpyro.sample('y_star',dist.TruncatedNormal(loc = latent_mean, scale = jnp.sqrt(var_error), high = jnp.where(z==0, 0, float('Inf')) ) )
+    var_genetic = jnp.mean((latent_mean)**2) - jnp.mean((latent_mean))**2
+    #var_genetic = torch.sum(torch.abs(beta)**2)
+    h = var_genetic/(var_genetic+var_error)
+    # Sampling
+    # with numpyro.plate("data", x.shape[0]):
+    #     numpyro.sample("obs", tobit_dist(latent_mean, jnp.sqrt(var_error)), obs = z)
+    with numpyro.plate("data", x.shape[0]):
+        numpyro.sample("obs", tobit_dist(latent_mean, jnp.sqrt(var_error)), obs = z)
     return h
