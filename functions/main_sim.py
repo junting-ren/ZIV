91417465d5d4a0120d93baa88a88ac8ab6f51284
juntingren <59756098+junting-ren@users.noreply.github.com>
2023-04-22 09:51:28 -0700

    Update main_sim.py
    Changed the saved path of the simulation result

--- functions/main_sim.py
+++ functions/main_sim.py
@@ -4,5 +4,5 @@ from sim_helper import sim_helper
 
 
 if __name__ == '__main__':
-    sim_ = sim_helper( n_sim = 2, heritability_l = [0.5,0.3], percent_causal_l = [0.1], beta_var_l = [1], image_modality = 'tfmri_list', random_seed = 1, path = '../simulation')
+    sim_ = sim_helper( n_sim = 2, heritability_l = [0.5,0.3], percent_causal_l = [0.1], beta_var_l = [1], image_modality = 'tfmri_list', random_seed = 1, path = '../simulation_result')
     sim_.full_run()
