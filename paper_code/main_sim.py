4366341e315a24efe405e2e0a8d930c148879558
juntingren <59756098+junting-ren@users.noreply.github.com>
2023-08-14 07:52:58 -0400

    Update main_sim.py
    simple fix

--- paper_code/main_sim.py
+++ paper_code/main_sim.py
@@ -4,8 +4,8 @@ from sim_helper import sim_helper
 
 if __name__ == '__main__':
     # main simulation using the full dataset
-    sim_ = sim_helper( n_sim = 500, heritability_l = [0.8, 0.5, 0.25,0.05], percent_causal_l = [0.1,0.05, 0.01,0.005], beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = '../simulation_result')
+    sim_ = sim_helper( n_sim = 500, heritability_l = [0.8, 0.5, 0.25,0.05], percent_causal_l = [0.1,0.05, 0.01,0.005], beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './')
     sim_.full_run()
     # supporting simulation using sub dataset for comparison between VI and MCMC
-    sim_ = sim_helper( n_sim = 500, heritability_l = [0.5], percent_causal_l = [0.1], beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = '../simulation_result_compare_mcmc', compare_mcmc = True, n_sub_l = [1000], p_sub_l = [50,100,200,500])
+    sim_ = sim_helper( n_sim = 500, heritability_l = [0.5], percent_causal_l = [0.1], beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './', compare_mcmc = True, n_sub_l = [1000], p_sub_l = [50,100,200,500])
     sim_.full_run()
