73b62c0951f5cc42af6e2ee2a805e77ac219b22d
juntingren <59756098+junting-ren@users.noreply.github.com>
2023-12-23 10:03:22 -0500

    Update main_sim.py
    Updated the simulation parameters for the simulated data

--- paper_code/main_sim.py
+++ paper_code/main_sim.py
@@ -1,11 +1,22 @@
 import sys
 sys.path.insert(1,'../src_simulation/')
 from sim_helper import sim_helper
-
+sim_data_indictor = True
 if __name__ == '__main__':
-    # main simulation using the full dataset
-    sim_ = sim_helper( n_sim = 500, heritability_l = [0.8, 0.5, 0.25,0.05], percent_causal_l = [0.1,0.05, 0.01,0.005], beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './')
-    sim_.full_run()
-    # supporting simulation using sub dataset for comparison between VI and MCMC
-    sim_ = sim_helper( n_sim = 500, heritability_l = [0.5], percent_causal_l = [0.1], beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './', compare_mcmc = True, n_sub_l = [1000], p_sub_l = [50,100,200,500])
-    sim_.full_run()
+    if sim_data_indictor:
+        sim_ = sim_helper( n_sim = 10, heritability_l = [0.5, 0.25,0.05],
+                          percent_causal_l = [0.1], 
+                          beta_var_l = [0.1], image_modality = None, random_seed = 1, 
+                          path = './', compare_mcmc = True, n_sub_l = [500,1000,2000], 
+                          p_sub_l = [10,50,100], sim_data = True, rho_l = [0,0.2,0.5])
+        sim_.full_run()
+    else:
+        # main simulation using the full dataset from ABCD
+        sim_ = sim_helper( n_sim = 500, heritability_l = [0.8, 0.5, 0.25,0.05], percent_causal_l = [0.1,0.05, 0.01,0.005], 
+                        beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './', sim_data = False)
+        sim_.full_run()
+        # supporting simulation using sub dataset for comparison between VI and MCMC
+        sim_ = sim_helper( n_sim = 500, heritability_l = [0.5], percent_causal_l = [0.1], beta_var_l = [0.1], 
+                        image_modality = 'tfmri_list', random_seed = 1, path = './', compare_mcmc = True, 
+                        n_sub_l = [1000], p_sub_l = [50,100,200,500], sim_data = False)
+        sim_.full_run()
