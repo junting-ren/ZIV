467075fce5a63a360da3df917c50075c1642a14d
juntingren <59756098+junting-ren@users.noreply.github.com>
2024-01-01 11:57:09 -0500

    Update main_sim.py
    Updated for sever simulation

--- paper_code/main_sim.py
+++ paper_code/main_sim.py
@@ -1,25 +1,23 @@
 import sys
 sys.path.insert(1,'../src_simulation/')
 from sim_helper import sim_helper
-sim_data_indictor = True
 compare_mcmc = True
 if __name__ == '__main__':
-    if sim_data_indictor:
-        sim_ = sim_helper(n_sim = 100, heritability_l = [0.8, 0.5, 0.25],
-                          percent_causal_l = [0.1,0.3], 
-                          beta_var_l = [0.1], image_modality = None, random_seed = 1, 
-                          path = './', compare_mcmc = compare_mcmc, n_sub_l = [1000,2000,4000], 
-                          p_sub_l = [400], sim_data = True, rho_l = [0], 
-                          linear_outcome_l = [True, False],
-                          file_suffix = "sim_X"
-                          )
-        sim_.full_run()
-    else:
-        # main simulation using the full dataset from ABCD
-        sim_ = sim_helper(n_sim = 100, heritability_l = [0.8, 0.5, 0.25], percent_causal_l = [0.1,0.05, 0.01,0.005], 
+    # sim_ = sim_helper(n_sim = 200, heritability_l = [0.8, 0.5, 0.25],
+    #                       percent_causal_l = [0.1,0.3], 
+    #                       beta_var_l = [0.1], image_modality = None, random_seed = 1, 
+    #                       path = './', compare_mcmc = compare_mcmc, n_sub_l = [200,400,1000,2000,4000], 
+    #                       p_sub_l = [400], sim_data = True, rho_l = [0], 
+    #                       linear_outcome_l = [True, False],
+    #                       file_suffix = "sim_X"
+    #                       )
+    # sim_.full_run()
+    # main simulation using the full dataset from ABCD
+    sim_real_X = sim_helper(n_sim = 200, heritability_l = [0.8, 0.5, 0.25], percent_causal_l = [0.1,0.05, 0.01,0.005], 
                         beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './', 
                         sim_data = False, compare_mcmc = compare_mcmc,
                         file_suffix = "real_X",
-                        data_path = '/Users/juntingren/Desktop/L0_VI_Bayesian_full_experimentation_code/dataset/'
+                        linear_outcome_l = [True, False],
+                        data_path = '../dataset/'
                         )
-        sim_.full_run()
+    sim_real_X.full_run()
