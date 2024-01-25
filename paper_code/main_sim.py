89d0a819c763f45b6945aa1fe4b9c83a627ae0bd
juntingren <59756098+junting-ren@users.noreply.github.com>
2024-01-24 22:42:05 -0500

    Update main_sim.py
    Updated to the latest simulation setup

--- paper_code/main_sim.py
+++ paper_code/main_sim.py
@@ -1,12 +1,11 @@
 import sys
 sys.path.insert(1,'../src_simulation/')
 from sim_helper import sim_helper
-compare_mcmc = True
 if __name__ == '__main__':
     sim_ = sim_helper(n_sim = 200, heritability_l = [0.8, 0.5, 0.25],
                           percent_causal_l = [0.1,0.3], 
                           beta_var_l = [0.1], image_modality = None, random_seed = 1, 
-                          path = './', compare_mcmc = compare_mcmc, n_sub_l = [200,400,1000,2000,4000], 
+                          path = './', compare_mcmc = True, n_sub_l = [200,400,1000,2000,4000], 
                           p_sub_l = [400], sim_data = True, rho_l = [0], 
                           linear_outcome_l = [True, False],
                           file_suffix = "sim_X"
@@ -15,7 +14,7 @@ if __name__ == '__main__':
     # main simulation using the full dataset from ABCD
     sim_real_X = sim_helper(n_sim = 200, heritability_l = [0.8, 0.5, 0.25], percent_causal_l = [0.1,0.05, 0.01,0.005], 
                         beta_var_l = [0.1], image_modality = 'tfmri_list', random_seed = 1, path = './', 
-                        sim_data = False, compare_mcmc = compare_mcmc,
+                        sim_data = False, compare_mcmc = False,
                         file_suffix = "real_X",
                         linear_outcome_l = [True, False],
                         data_path = '../dataset/'
